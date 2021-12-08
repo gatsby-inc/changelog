46b4ea73cd92ce8f5efaac0ce25054bc5801b4bd
pauliescanlon <paul.scanlon@gatsbyjs.com>
2021-12-08 11:36:00 -0500

    feat: add loading spinner

--- src/pages/twitter-feed.js
+++ src/pages/twitter-feed.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
 
+import Loading from '../components/loading';
+
 const Page = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const [tweets, setTweets] = useState([]);
 
   useEffect(() => {
@@ -14,6 +17,7 @@ const Page = () => {
       const data = await response.json();
 
       setTweets(data.tweets);
+      setIsLoading(false);
     };
 
     getTweets();
@@ -21,16 +25,22 @@ const Page = () => {
 
   return (
     <div>
-      <ul>
-        {tweets.map((tweet, index) => {
-          const { text } = tweet;
-          return (
-            <li key={index} className="shadow p-8 bg-white rounded-lg">
-              <pre className="font-sans whitespace-pre-wrap">{text}</pre>
-            </li>
-          );
-        })}
-      </ul>
+      {isLoading ? (
+        <div className="flex place-content-center">
+          <Loading />
+        </div>
+      ) : (
+        <ul>
+          {tweets.map((tweet, index) => {
+            const { text } = tweet;
+            return (
+              <li key={index} className="shadow p-8 bg-white rounded-lg">
+                <pre className="font-sans whitespace-pre-wrap">{text}</pre>
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 };
