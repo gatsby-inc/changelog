be3ce9a19a0891c44cedb98b5ad1f91df979d72c
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-09-03 17:52:27 +0100

    feat: add changelog line chart

--- src/pages/github-commits.js
+++ src/pages/github-commits.js
@@ -6,18 +6,27 @@ import LineChart from '../components/line-chart';
 
 const Page = () => {
   const [isLoading, setIsLoading] = useState(true);
-  const [response, setResponse] = useState({});
+  const [gatsby, setGatsby] = useState(null);
+  const [changelog, setChangelog] = useState(null);
 
   useEffect(() => {
     const getCommits = async () => {
-      const response = await (
+      const gatsby = await (
         await fetch('/api/github/commits', {
           method: 'POST',
           body: JSON.stringify({ owner: 'gatsbyjs', repository: 'gatsby' })
         })
       ).json();
 
-      setResponse(response);
+      const changelog = await (
+        await fetch('/api/github/commits', {
+          method: 'POST',
+          body: JSON.stringify({ owner: 'gatsby-inc', repository: 'changelog' })
+        })
+      ).json();
+
+      setGatsby(gatsby);
+      setChangelog(changelog);
       setIsLoading(false);
     };
 
@@ -29,19 +38,38 @@ const Page = () => {
       {isLoading ? (
         <Loading />
       ) : (
-        <div className="w-full">
-          <h5 className="mb-3 font-bold text-gray-600">12 Month Commit Activity</h5>
-          <LineChart data={response} />
-          <small>
-            <a
-              href="https://github.com/gatsbyjs/gatsby"
-              target="_blank"
-              rel="noreferrer"
-              className="text-xs text-brand-primary hover:text-brand-secondary"
-            >
-              github.com/gatsbyjs/gatsby
-            </a>
-          </small>
+        <div className="w-full grid gap-8">
+          <h1 className="text-3xl font-black text-brand-primary">12 Month Commit Activity</h1>
+          <div className="grid gap-16">
+            <div className="grid gap-4">
+              <h2 className="text-lg font-bold text-gray-800">Gatsby</h2>
+              <LineChart data={gatsby} />
+              <small className="text-xs">
+                <a
+                  href="https://github.com/gatsbyjs/gatsby"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="text-brand-primary hover:text-brand-secondary"
+                >
+                  github.com/gatsbyjs/gatsby
+                </a>
+              </small>
+            </div>
+            <div className="grid gap-4">
+              <h2 className="text-lg font-bold text-gray-800">Changelog</h2>
+              <LineChart data={changelog} />
+              <small className="text-xs">
+                <a
+                  href="https://github.com/gatsby-inc/changelog"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="text-brand-primary hover:text-brand-secondary"
+                >
+                  github.com/gatsby-inc/changelog
+                </a>
+              </small>
+            </div>
+          </div>
         </div>
       )}
     </Fragment>
